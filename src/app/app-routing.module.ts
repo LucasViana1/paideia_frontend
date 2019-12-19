3ecf1452f196a2f2cf70db46d4609b79b9ba3dbc
Lucas Viana Cunha <lucasviana112@gmail.com>
2019-12-19 20:07:40 -0300

    arquivos iniciais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { CadastroComponent } from "./pages/public/Cadastro/cadastro/cadastro.component";
+import { LoginComponent } from "./pages/public/Acesso/login/login.component";
+import { InicioComponent } from "./pages/public/Inicio/inicio/inicio.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "inicio", component: InicioComponent },
+  { path: "acesso", component: LoginComponent },
+  { path: "cadastro", component: CadastroComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/public/Inicio/inicio/inicio.component';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
+import { LoginComponent } from './pages/public/Acesso/login/login.component';
+import { CadastroComponent } from './pages/public/Cadastro/cadastro/cadastro.component';
+import { ContatoComponent } from './pages/public/Contato/contato/contato.component';
+import { NoticiasComponent } from './pages/public/Noticias/noticias/noticias.component';
+import { PerguntasComponent } from './pages/public/Perguntas/perguntas/perguntas.component';
+import { SobreComponent } from './pages/public/Sobre/sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    LoginComponent,
+    CadastroComponent,
+    ContatoComponent,
+    NoticiasComponent,
+    PerguntasComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
