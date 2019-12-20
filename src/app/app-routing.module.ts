c2e81c818137e1953788a64e17f202d7ed38a9e9
Lucas Viana Cunha <lucasviana112@gmail.com>
2019-12-20 13:24:33 -0300

    assets img, info e configuracao bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,7 @@
+import { SobreComponent } from "./pages/public/Sobre/sobre/sobre.component";
+import { PerguntasComponent } from "./pages/public/Perguntas/perguntas/perguntas.component";
+import { NoticiasComponent } from "./pages/public/Noticias/noticias/noticias.component";
+import { ContatoComponent } from "./pages/public/Contato/contato/contato.component";
 import { CadastroComponent } from "./pages/public/Cadastro/cadastro/cadastro.component";
 import { LoginComponent } from "./pages/public/Acesso/login/login.component";
 import { InicioComponent } from "./pages/public/Inicio/inicio/inicio.component";
@@ -5,9 +9,14 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-  { path: "inicio", component: InicioComponent },
+  { path: "", redirectTo: "/inicio", pathMatch: "full" },
   { path: "acesso", component: LoginComponent },
-  { path: "cadastro", component: CadastroComponent }
+  { path: "cadastro", component: CadastroComponent },
+  { path: "contato", component: ContatoComponent },
+  { path: "inicio", component: InicioComponent },
+  { path: "noticias", component: NoticiasComponent },
+  { path: "perguntas", component: PerguntasComponent },
+  { path: "sobre", component: SobreComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { InicioComponent } from './pages/public/Inicio/inicio/inicio.component';
-import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
-import { RodapeComponent } from './components/rodape/rodape.component';
-import { LoginComponent } from './pages/public/Acesso/login/login.component';
-import { CadastroComponent } from './pages/public/Cadastro/cadastro/cadastro.component';
-import { ContatoComponent } from './pages/public/Contato/contato/contato.component';
-import { NoticiasComponent } from './pages/public/Noticias/noticias/noticias.component';
-import { PerguntasComponent } from './pages/public/Perguntas/perguntas/perguntas.component';
-import { SobreComponent } from './pages/public/Sobre/sobre/sobre.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { InicioComponent } from "./pages/public/Inicio/inicio/inicio.component";
+import { CabecalhoComponent } from "./components/cabecalho/cabecalho.component";
+import { RodapeComponent } from "./components/rodape/rodape.component";
+import { LoginComponent } from "./pages/public/Acesso/login/login.component";
+import { CadastroComponent } from "./pages/public/Cadastro/cadastro/cadastro.component";
+import { ContatoComponent } from "./pages/public/Contato/contato/contato.component";
+import { NoticiasComponent } from "./pages/public/Noticias/noticias/noticias.component";
+import { PerguntasComponent } from "./pages/public/Perguntas/perguntas/perguntas.component";
+import { SobreComponent } from "./pages/public/Sobre/sobre/sobre.component";
+import { SlidesComponent } from './pages/public/Inicio/slides/slides.component';
+import { ProcessoSeletivoComponent } from './pages/public/Inicio/processo-seletivo/processo-seletivo.component';
+import { DepoimentosComponent } from './pages/public/Inicio/depoimentos/depoimentos.component';
 
 @NgModule({
   declarations: [
@@ -24,13 +28,13 @@ import { SobreComponent } from './pages/public/Sobre/sobre/sobre.component';
     ContatoComponent,
     NoticiasComponent,
     PerguntasComponent,
-    SobreComponent
+    SobreComponent,
+    SlidesComponent,
+    ProcessoSeletivoComponent,
+    DepoimentosComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [NgbModule, BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
