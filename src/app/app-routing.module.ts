e05506c4ea9352f9c4a3a5bebe56cc6354e26a72
Lucas Viana Cunha <lucasviana112@gmail.com>
2019-12-30 16:37:43 -0300

    insercao das rotas filhas de adm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,7 @@
+import { InscritosComponent } from "./pages/admin/inscritos/inscritos.component";
+import { CompletoComponent } from "./pages/admin/completo/completo.component";
+import { DetalhesComponent } from "./pages/admin/detalhes/detalhes.component";
+import { AdmComponent } from "./pages/admin/Administrador/adm/adm.component";
 import { SobreComponent } from "./pages/public/Sobre/sobre/sobre.component";
 import { PerguntasComponent } from "./pages/public/Perguntas/perguntas/perguntas.component";
 import { NoticiasComponent } from "./pages/public/Noticias/noticias/noticias.component";
@@ -7,11 +11,11 @@ import { LoginComponent } from "./pages/public/Acesso/login/login.component";
 import { InicioComponent } from "./pages/public/Inicio/inicio/inicio.component";
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { DepoimentosTodosComponent } from './pages/public/Inicio/depoimentos-todos/depoimentos-todos.component';
-import { InscricaoComponent } from './pages/subscriber/Inscricao/inscricao/inscricao.component';
-import { PessoalComponent } from './pages/subscriber/Inscricao/pessoal/pessoal.component';
-import { TermosComponent } from './pages/subscriber/Inscricao/termos/termos.component';
-import { ArquivosComponent } from './pages/subscriber/Inscricao/arquivos/arquivos.component';
+import { DepoimentosTodosComponent } from "./pages/public/Inicio/depoimentos-todos/depoimentos-todos.component";
+import { InscricaoComponent } from "./pages/subscriber/Inscricao/inscricao/inscricao.component";
+import { PessoalComponent } from "./pages/subscriber/Inscricao/pessoal/pessoal.component";
+import { TermosComponent } from "./pages/subscriber/Inscricao/termos/termos.component";
+import { ArquivosComponent } from "./pages/subscriber/Inscricao/arquivos/arquivos.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/inicio", pathMatch: "full" },
@@ -24,11 +28,24 @@ const routes: Routes = [
   { path: "perguntas", component: PerguntasComponent },
   { path: "sobre", component: SobreComponent },
   { path: "depoimentos", component: DepoimentosTodosComponent },
-  { path: "inscricao", component: InscricaoComponent, children: [
-    {path: "termos", component: TermosComponent},
-    {path: "pessoal", component: PessoalComponent},
-    {path: "arquivos", component: ArquivosComponent},
-  ]}, // rota com guardas
+  {
+    path: "inscricao",
+    component: InscricaoComponent,
+    children: [
+      { path: "termos", component: TermosComponent },
+      { path: "pessoal", component: PessoalComponent },
+      { path: "arquivos", component: ArquivosComponent }
+    ]
+  }, // rota com guardas
+  {
+    path: "adm",
+    component: AdmComponent,
+    children: [
+      { path: "inscritos", component: InscritosComponent },
+      { path: "detalhes", component: DetalhesComponent },
+      { path: "completo", component: CompletoComponent }
+    ]
+  } // rota com guardas
 ];
 
 @NgModule({
