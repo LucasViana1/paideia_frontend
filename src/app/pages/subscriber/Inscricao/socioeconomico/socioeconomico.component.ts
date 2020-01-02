5be1c655953d0518316a1a4c85d0acd8d06c7db1
Lucas Viana Cunha <lucasviana112@gmail.com>
2020-01-02 20:19:54 -0300

    ajuste formulario socioeconomico

--- src/app/pages/subscriber/Inscricao/socioeconomico/socioeconomico.component.ts
+++ src/app/pages/subscriber/Inscricao/socioeconomico/socioeconomico.component.ts
@@ -10,7 +10,32 @@ export class SocioeconomicoComponent implements OnInit {
   constructor(public fb: FormBuilder) {}
 
   formSocio = this.fb.group({
-    teste: [""]
+    sexo: [""],
+    mora_tempo: [""],
+    local: [""],
+    mora_pessoas_perentesco: [""], // talvez remover
+    mora_pessoas_qtd: [""],
+    mora_tipo: [""],
+    trab_candidato: [""],
+    trab_pais: [""],
+    fundamental: [""],
+    medio: [""],
+    ler: [""],
+    ler_qtd: [""],
+    informado: [""],
+    internet: [""],
+    fez_vestibular: [""],
+    trab_estudo: [""],
+    motivo_estudar: [""],
+    transporte: [""],
+    // checkbox
+    televisao_atividade: [""],
+    musica_atividade: [""],
+    cinema_atividade: [""],
+    leitura_atividade: [""],
+    internet_atividade: [""],
+    esporte_atividade: [""],
+    nenhuma_atividade: [""]
   });
 
   onSubmit() {
