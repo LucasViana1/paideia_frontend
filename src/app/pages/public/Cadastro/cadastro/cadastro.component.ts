1c7d97075b2298feafc3623472e606474efa0319
Lucas Viana Cunha <lucasviana112@gmail.com>
2020-01-02 14:11:24 -0300

    implementacao da tela de cadastro

--- src/app/pages/public/Cadastro/cadastro/cadastro.component.ts
+++ src/app/pages/public/Cadastro/cadastro/cadastro.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
 @Component({
-  selector: 'app-cadastro',
-  templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  selector: "app-cadastro",
+  templateUrl: "./cadastro.component.html",
+  styleUrls: ["./cadastro.component.css"]
 })
 export class CadastroComponent implements OnInit {
+  constructor(public fb: FormBuilder) {}
 
-  constructor() { }
+  formCadastro = this.fb.group({
+    nome: [""],
+    sobrenome: [""],
+    email: [""],
+    senha: [""],
+    confirmaSenha: [""]
+  });
 
-  ngOnInit() {
+  onSubmit() {
+    let form = this.formCadastro.value;
+    alert(JSON.stringify(form));
   }
 
+  ngOnInit() {}
 }
