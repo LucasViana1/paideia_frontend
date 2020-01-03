218208795c5a27691b9d0afb8d78fa281b4de5f4
Lucas Viana Cunha <lucasviana112@gmail.com>
2020-01-03 17:31:18 -0300

    testes inicias de requisicao com post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,27 +13,29 @@ import { ContatoComponent } from "./pages/public/Contato/contato/contato.compone
 import { NoticiasComponent } from "./pages/public/Noticias/noticias/noticias.component";
 import { PerguntasComponent } from "./pages/public/Perguntas/perguntas/perguntas.component";
 import { SobreComponent } from "./pages/public/Sobre/sobre/sobre.component";
-import { SlidesComponent } from './pages/public/Inicio/slides/slides.component';
-import { ProcessoSeletivoComponent } from './pages/public/Inicio/processo-seletivo/processo-seletivo.component';
-import { DepoimentosComponent } from './pages/public/Inicio/depoimentos/depoimentos.component';
-import { DepoimentosTodosComponent } from './pages/public/Inicio/depoimentos-todos/depoimentos-todos.component';
-import { AdmComponent } from './pages/admin/Administrador/adm/adm.component';
-import { FormularioComponent } from './pages/subscriber/Inscricao/formulario/formulario.component';
-import { SimuladoComponent } from './pages/subscriber/Simulado/simulado/simulado.component';
-import { InscricaoComponent } from './pages/subscriber/Inscricao/inscricao/inscricao.component';
+import { SlidesComponent } from "./pages/public/Inicio/slides/slides.component";
+import { ProcessoSeletivoComponent } from "./pages/public/Inicio/processo-seletivo/processo-seletivo.component";
+import { DepoimentosComponent } from "./pages/public/Inicio/depoimentos/depoimentos.component";
+import { DepoimentosTodosComponent } from "./pages/public/Inicio/depoimentos-todos/depoimentos-todos.component";
+import { AdmComponent } from "./pages/admin/Administrador/adm/adm.component";
+import { FormularioComponent } from "./pages/subscriber/Inscricao/formulario/formulario.component";
+import { SimuladoComponent } from "./pages/subscriber/Simulado/simulado/simulado.component";
+import { InscricaoComponent } from "./pages/subscriber/Inscricao/inscricao/inscricao.component";
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { PessoalComponent } from './pages/subscriber/Inscricao/pessoal/pessoal.component';
-import { TermosComponent } from './pages/subscriber/Inscricao/termos/termos.component';
+import { PessoalComponent } from "./pages/subscriber/Inscricao/pessoal/pessoal.component";
+import { TermosComponent } from "./pages/subscriber/Inscricao/termos/termos.component";
 
-import { TextMaskModule } from 'angular2-text-mask';
-import { ArquivosComponent } from './pages/subscriber/Inscricao/arquivos/arquivos.component';
-import { DetalhesComponent } from './pages/admin/detalhes/detalhes.component';
-import { CompletoComponent } from './pages/admin/completo/completo.component';
-import { InscritosComponent } from './pages/admin/inscritos/inscritos.component';
-import { RecuperacaoComponent } from './pages/public/Acesso/recuperacao/recuperacao.component';
-import { SocioeconomicoComponent } from './pages/subscriber/Inscricao/socioeconomico/socioeconomico.component';
+import { TextMaskModule } from "angular2-text-mask";
+import { ArquivosComponent } from "./pages/subscriber/Inscricao/arquivos/arquivos.component";
+import { DetalhesComponent } from "./pages/admin/detalhes/detalhes.component";
+import { CompletoComponent } from "./pages/admin/completo/completo.component";
+import { InscritosComponent } from "./pages/admin/inscritos/inscritos.component";
+import { RecuperacaoComponent } from "./pages/public/Acesso/recuperacao/recuperacao.component";
+import { SocioeconomicoComponent } from "./pages/subscriber/Inscricao/socioeconomico/socioeconomico.component";
+
+import { HttpClientModule } from "@angular/common/http";
 @NgModule({
   declarations: [
     AppComponent,
@@ -63,7 +65,15 @@ import { SocioeconomicoComponent } from './pages/subscriber/Inscricao/socioecono
     RecuperacaoComponent,
     SocioeconomicoComponent
   ],
-  imports: [NgbModule, BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, TextMaskModule],
+  imports: [
+    NgbModule,
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TextMaskModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/pages/public/Cadastro/cadastro/cadastro.component.ts
+++ src/app/pages/public/Cadastro/cadastro/cadastro.component.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
 @Component({
   selector: "app-cadastro",
   templateUrl: "./cadastro.component.html",
   styleUrls: ["./cadastro.component.css"]
 })
 export class CadastroComponent implements OnInit {
-  constructor(public fb: FormBuilder) {}
+  postData = {
+    nome: "teste nome2",
+    sobrenome: "teste nosobrenomeme2",
+    email: "teste email2",
+    senha: "teste senha2"
+  };
+
+  url = "http://localhost:3000/cadastraUser";
+
+  constructor(public fb: FormBuilder, private http: HttpClient) {
+    this.http
+      .post(this.url, this.postData)
+      .toPromise()
+      .then(data => {
+        console.log("ok");
+      });
+  }
 
   formCadastro = this.fb.group({
     nome: [""],
