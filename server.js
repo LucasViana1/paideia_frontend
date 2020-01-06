2e060e9f92936b3b45f77d47c57c0c1af6bb9b91
Lucas Viana <lucasviana112@gmail.com>
2020-01-06 06:52:01 -0300

    alteracoes para implantacao na heroku

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
-const express = require('express')
-//var enforce = require('express-sslify');
-//const serveStatic = require('serve-static')
-//const path = require('path')
+//Importa as dependências que acabamos de instalar
+const express = require('express');
+const path = require('path');
 
-const app = express()
+const app = express();
 
-//app.use('/', serveStatic(path.join(__dirname, '/dist')))
-//app.use(enforce.HTTPS({ trustProtoHeader: true }));
-app.use(express.static(__dirname + '/dist'));
-/*app.use('/teste', function(){
-    console.log("oi")
-})*/
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static(__dirname + '/dist/frontend'));
 
-const port = process.env.PORT || 8080
-app.listen(port)
+app.get('/*', function(req,res) {
 
-console.log('Web server na porta: '+port)
+res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
+});
+
+// Inicia a aplicação pela porta configurada
+app.listen(process.env.PORT || 8080);
+
+// const express = require('express')
+// //var enforce = require('express-sslify');
+// //const serveStatic = require('serve-static')
+// //const path = require('path')
+
+// const app = express()
+
+// //app.use('/', serveStatic(path.join(__dirname, '/dist')))
+// //app.use(enforce.HTTPS({ trustProtoHeader: true }));
+// app.use(express.static(__dirname + '/dist'));
+// /*app.use('/teste', function(){
+//     console.log("oi")
+// })*/
+
+// const port = process.env.PORT || 8080
+// app.listen(port)
+
+// console.log('Web server na porta: '+port)
