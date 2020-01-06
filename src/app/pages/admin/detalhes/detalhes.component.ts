312785a94a1c7f31b98f2acfeeea643d246ed44e
Lucas Viana <lucasviana112@gmail.com>
2020-01-06 06:19:45 -0300

    ajustes tela de detalhes do candidato

--- src/app/pages/admin/detalhes/detalhes.component.ts
+++ src/app/pages/admin/detalhes/detalhes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AdmAPIService} from '../../../services/adm-api.service';
+import { ActivatedRoute } from '@angular/router';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-detalhes',
@@ -6,10 +9,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detalhes.component.css']
 })
 export class DetalhesComponent implements OnInit {
+  listagem:any;
+  rgView:boolean = false;
+  cpfView:boolean = false;
+  cidadaoView:boolean = false;
+  enderecoView:boolean = false;
+  historicoView:boolean = false;
+  bolsaView:boolean = false;
 
-  constructor() { }
+  constructor(private service: AdmAPIService, private routeActivated: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    if(window.localStorage.getItem('nivel') !== '1'){
+      this.router.navigate(['/inicio']);
+    }
+    this.routeActivated.params.subscribe( parametros => {
+      if (parametros['id']) {
+        console.log('params')
+        console.log(parametros['id'])
+        this.detalhes(parametros['id']);
+      }
+    });
+  }
+
+  detalhes(id:any){
+    this.service.detalhesInscritos(id).subscribe(dados => this.listagem = dados);
+  }
+
+  verImg(tipo:any){
+    console.log(tipo)
+    switch(tipo){
+      case 'RG':
+        this.ocultaTodasImg();
+        (<HTMLSelectElement>document.getElementById('RG')).style.display = 'initial';
+        break;
+      case 'CPF':
+        this.ocultaTodasImg();
+        (<HTMLSelectElement>document.getElementById('CPF')).style.display = 'initial';
+        break;
+      case 'ENDERECO':
+        this.ocultaTodasImg();
+        (<HTMLSelectElement>document.getElementById('ENDERECO')).style.display = 'initial';
+        break;
+      case 'HISTORICO':
+        this.ocultaTodasImg();
+        (<HTMLSelectElement>document.getElementById('HISTORICO')).style.display = 'initial';
+        break;
+      case 'CIDADAO':
+        this.ocultaTodasImg();
+        (<HTMLSelectElement>document.getElementById('CIDADAO')).style.display = 'initial';
+        break;
+      case 'BOLSA':
+        this.ocultaTodasImg();
+        (<HTMLSelectElement>document.getElementById('BOLSA')).style.display = 'initial';
+        break;
+    }
+  }
+
+  ocultaTodasImg(){
+    (<HTMLSelectElement>document.getElementById('RG')).style.display = 'none';
+    (<HTMLSelectElement>document.getElementById('CPF')).style.display = 'none';
+    (<HTMLSelectElement>document.getElementById('ENDERECO')).style.display = 'none';
+    (<HTMLSelectElement>document.getElementById('HISTORICO')).style.display = 'none';
+    (<HTMLSelectElement>document.getElementById('CIDADAO')).style.display = 'none';
+    (<HTMLSelectElement>document.getElementById('BOLSA')).style.display = 'none';
   }
 
 }
