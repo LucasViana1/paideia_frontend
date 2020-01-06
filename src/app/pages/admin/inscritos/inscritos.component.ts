c7dd5aecc8d3a927a1d859133d7524fb24eb8385
Lucas Viana <lucasviana112@gmail.com>
2020-01-06 06:20:32 -0300

    alteracoes nas funcionalidades do adm

--- src/app/pages/admin/inscritos/inscritos.component.ts
+++ src/app/pages/admin/inscritos/inscritos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AdmAPIService} from '../../../services/adm-api.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-inscritos',
@@ -7,11 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InscritosComponent implements OnInit {
   nome:string;
+  inscritos: Array<any>;
 
-  constructor() { }
+  constructor(private service: AdmAPIService, private router: Router) { }
 
   ngOnInit() {
+    if(window.localStorage.getItem('nivel') !== '1'){
+      this.router.navigate(['/inicio']);
+    }
+    this.listar();
     this.nome = window.localStorage.getItem('nome');
+  }
+
+  listar(){
+    // this.service.listaInscritos().subscribe(dados => this.inscritos = dados)
+    // this.service.listaInscritos().subscribe(dados => console.log(dados))
+    this.service.listaInscritos().subscribe(dados => this.inscritos = dados);
   }
 
 }
