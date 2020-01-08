ce1aeebf8a5b1cbd94541e3cc756e5b4f690b199
Lucas Viana Cunha <lucasviana112@gmail.com>
2020-01-08 17:45:50 -0300

    tentativa de visualizacao de pdf

--- src/app/pages/admin/detalhes/detalhes.component.ts
+++ src/app/pages/admin/detalhes/detalhes.component.ts
@@ -3,6 +3,11 @@ import { AdmAPIService } from "../../../services/adm-api.service";
 import { ActivatedRoute } from "@angular/router";
 import { Router } from "@angular/router";
 import { ThrowStmt } from "@angular/compiler";
+import {
+  DomSanitizer,
+  SafeResourceUrl,
+  SafeUrl
+} from "@angular/platform-browser";
 
 @Component({
   selector: "app-detalhes",
@@ -21,7 +26,8 @@ export class DetalhesComponent implements OnInit {
   constructor(
     private service: AdmAPIService,
     private routeActivated: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private _sanitizationService: DomSanitizer
   ) {}
 
   ngOnInit() {
@@ -103,5 +109,11 @@ export class DetalhesComponent implements OnInit {
     //   "none";
     // (<HTMLSelectElement>document.getElementById("BOLSA")).style.display =
     //   "none";
+  }
+
+  validationSanitizer(yourSrc: any) {
+    // var imgSrc = this._sanitizationService.bypassSecurityTrustUrl('data:image/png;base64,' + yourSrc);
+    var imgSrc = this._sanitizationService.bypassSecurityTrustUrl(yourSrc);
+    return imgSrc;
   }
 }
