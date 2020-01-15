8b16796560842a26f6d7c4ed45816cd17b3f1542
Lucas Viana Cunha <lucasviana112@gmail.com>
2020-01-15 17:42:48 -0300

    continuacao template de cadastro de simulado

--- src/app/pages/admin/simulado-cadastro/simulado-cadastro.component.ts
+++ src/app/pages/admin/simulado-cadastro/simulado-cadastro.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
+// import {UsersAPIService} from '../../../../services/users-api.service';
+// INSERIR API PARA SIMULADOS
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-simulado-cadastro',
-  templateUrl: './simulado-cadastro.component.html',
-  styleUrls: ['./simulado-cadastro.component.css']
+  selector: "app-simulado-cadastro",
+  templateUrl: "./simulado-cadastro.component.html",
+  styleUrls: ["./simulado-cadastro.component.css"]
 })
 export class SimuladoCadastroComponent implements OnInit {
+  constructor(
+    public fb: FormBuilder,
+    /*private service: UsersAPIService,*/ private router: Router
+  ) {}
 
-  constructor() { }
+  formSimuladoCadastro = this.fb.group({
+    linkImg: [""],
+    enunciado: [""]
+  });
 
-  ngOnInit() {
+  onSubmit() {
+    const form = this.formSimuladoCadastro.value;
+    console.log(form);
   }
 
+  ngOnInit() {}
 }
