49698bce02c739b4fcd4321f667284bdfc051c13
Lucas Viana <lucasviana112@gmail.com>
2020-01-27 07:18:00 -0300

    tentativa de algoritmo para geração de resultado personalizado ao adm

--- src/app/pages/admin/simulado-resultado-completo/simulado-resultado-completo.component.ts
+++ src/app/pages/admin/simulado-resultado-completo/simulado-resultado-completo.component.ts
@@ -26,7 +26,40 @@ export class SimuladoResultadoCompletoComponent implements OnInit {
   }
   calculoAcertos() {
     let itens = this.listagem.dados;
+    let result = [];
+    let cont = 0;
+    let mat1 = '';
     console.log("itens");
     console.log(itens);
+    // percorre todas as linhas retornadas da api
+    for (let i = 0; i < this.listagem.dados.length; i++) {
+      if(i === 0) mat1 = this.listagem.dados[i].materia;
+      // verifica se a proxima linha será da msm materia analisada atualmente
+      // console.log('teste')
+      // let prox = i + 1;
+      // console.log(this.listagem.dados[prox].materia)
+
+      // let mat2 = this.listagem.dados[prox].materia;
+      // if (this.listagem.dados[i].materia === this.listagem.dados[i + 1].materia) {
+      // if (mat1 === mat2) {
+      // verifica se aluno acertou
+      if (this.listagem.dados[i].acertou === "s") {
+        cont++;
+      }
+      // }
+      // reseta infos para novo aluno, e armazena o resultado do antigo aluno
+      // else {
+      cont++;
+      result.push({
+        nome: `${this.listagem.dados[i].nome} ${this.listagem.dados[i].sobrenome}`,
+        materia: this.listagem.dados[i].materia,
+        acertos: cont
+      });
+      cont = 0;
+      // }
+      let mat1 = this.listagem.dados[i].materia;
+    }
+    console.log("result");
+    console.log(result);
   }
 }
