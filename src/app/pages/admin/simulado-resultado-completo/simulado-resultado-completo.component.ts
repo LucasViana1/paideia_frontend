c66a8d6052b6a4c8bf702dfa17ff91db43b1823e
Lucas Viana Cunha <lucasviana112@gmail.com>
2020-01-27 17:32:59 -0300

    ajuste visualização resultado simulado adm

--- src/app/pages/admin/simulado-resultado-completo/simulado-resultado-completo.component.ts
+++ src/app/pages/admin/simulado-resultado-completo/simulado-resultado-completo.component.ts
@@ -9,6 +9,20 @@ import { Router } from "@angular/router";
 })
 export class SimuladoResultadoCompletoComponent implements OnInit {
   listagem: any;
+  lista: any;
+  itens: any[] = [];
+  qtdFisica: number = 0;
+  qtdBiologia: number = 0;
+  qtdMatematica: number = 0;
+  qtdHistoria: number = 0;
+  qtdQuimica: number = 0;
+  qtdGeografia: number = 0;
+  qtdGramatica: number = 0;
+  qtdSociologia: number = 0;
+  qtdFilosofia: number = 0;
+  qtdArtes: number = 0;
+  qtdIngles: number = 0;
+  qtdTotal: number;
   constructor(private service: SimuladoAPIService, private router: Router) {}
 
   ngOnInit() {
@@ -25,41 +39,106 @@ export class SimuladoResultadoCompletoComponent implements OnInit {
     });
   }
   calculoAcertos() {
-    let itens = this.listagem.dados;
-    let result = [];
-    let cont = 0;
-    let mat1 = '';
-    console.log("itens");
-    console.log(itens);
-    // percorre todas as linhas retornadas da api
     for (let i = 0; i < this.listagem.dados.length; i++) {
-      if(i === 0) mat1 = this.listagem.dados[i].materia;
-      // verifica se a proxima linha será da msm materia analisada atualmente
-      // console.log('teste')
-      // let prox = i + 1;
-      // console.log(this.listagem.dados[prox].materia)
-
-      // let mat2 = this.listagem.dados[prox].materia;
-      // if (this.listagem.dados[i].materia === this.listagem.dados[i + 1].materia) {
-      // if (mat1 === mat2) {
-      // verifica se aluno acertou
-      if (this.listagem.dados[i].acertou === "s") {
-        cont++;
-      }
-      // }
-      // reseta infos para novo aluno, e armazena o resultado do antigo aluno
-      // else {
-      cont++;
-      result.push({
-        nome: `${this.listagem.dados[i].nome} ${this.listagem.dados[i].sobrenome}`,
-        materia: this.listagem.dados[i].materia,
-        acertos: cont
-      });
-      cont = 0;
-      // }
-      let mat1 = this.listagem.dados[i].materia;
+      this.getGabaritoSimples(
+        this.listagem.dados[i].idUser,
+        this.listagem.dados[i].matricula,
+        this.listagem.dados[i].nome,
+        this.listagem.dados[i].sobrenome
+      );
     }
-    console.log("result");
-    console.log(result);
+  }
+  getGabaritoSimples(id: any, matricula: any, nome: any, sobrenome: any) {
+    this.service.getGabaritoSimples(id).subscribe(resp => {
+      this.lista = resp;
+      // console.log(this.lista.dados[0]);
+      for (let i = 0; i < this.lista.dados.length; i++) {
+        switch (this.lista.dados[i].materia) {
+          case "Física":
+            if (this.lista.dados[i].acertou === "s") this.qtdFisica++;
+            break;
+          case "Biologia":
+            if (this.lista.dados[i].acertou === "s") this.qtdBiologia++;
+            break;
+          case "Matemática":
+            if (this.lista.dados[i].acertou === "s") this.qtdMatematica++;
+            break;
+          case "História":
+            if (this.lista.dados[i].acertou === "s") this.qtdHistoria++;
+            break;
+          case "Química":
+            if (this.lista.dados[i].acertou === "s") this.qtdQuimica++;
+            break;
+          case "Geografia":
+            if (this.lista.dados[i].acertou === "s") this.qtdGeografia++;
+            break;
+          case "Gramática":
+            if (this.lista.dados[i].acertou === "s") this.qtdGramatica++;
+            break;
+          case "Sociologia":
+            if (this.lista.dados[i].acertou === "s") this.qtdSociologia++;
+            break;
+          case "Filosofia":
+            if (this.lista.dados[i].acertou === "s") this.qtdFilosofia++;
+            break;
+          case "Artes":
+            if (this.lista.dados[i].acertou === "s") this.qtdArtes++;
+            break;
+          case "Inglês":
+            if (this.lista.dados[i].acertou === "s") {
+              this.qtdIngles++;
+              // console.log("this.qtdIngles");
+              // console.log(this.qtdIngles);
+            }
+            break;
+          default:
+            console.log("Falha ao identificar a matéria");
+            break;
+        }
+      }
+      console.log("barr teste");
+      // console.log("this.qtdBiologia");
+      // console.log(this.qtdBiologia);
+      this.qtdTotal =
+        this.qtdFisica +
+        this.qtdBiologia +
+        this.qtdMatematica +
+        this.qtdHistoria +
+        this.qtdQuimica +
+        this.qtdGeografia +
+        this.qtdGramatica +
+        this.qtdSociologia +
+        this.qtdFilosofia +
+        this.qtdArtes +
+        this.qtdIngles;
+      this.itens.push({
+        nome: `${nome} ${sobrenome}`,
+        matricula: matricula,
+        qtdFisica: this.qtdFisica,
+        qtdBiologia: this.qtdBiologia,
+        qtdMatematica: this.qtdMatematica,
+        qtdHistoria: this.qtdHistoria,
+        qtdQuimica: this.qtdQuimica,
+        qtdGeografia: this.qtdGeografia,
+        qtdGramatica: this.qtdGramatica,
+        qtdSociologia: this.qtdSociologia,
+        qtdFilosofia: this.qtdFilosofia,
+        qtdArtes: this.qtdArtes,
+        qtdIngles: this.qtdIngles,
+        qtdTotal: this.qtdTotal
+      });
+      this.qtdFisica = 0;
+      this.qtdBiologia = 0;
+      this.qtdMatematica = 0;
+      this.qtdHistoria = 0;
+      this.qtdQuimica = 0;
+      this.qtdGeografia = 0;
+      this.qtdGramatica = 0;
+      this.qtdSociologia = 0;
+      this.qtdFilosofia = 0;
+      this.qtdArtes = 0;
+      this.qtdIngles = 0;
+      this.qtdTotal = 0;
+    });
   }
 }
