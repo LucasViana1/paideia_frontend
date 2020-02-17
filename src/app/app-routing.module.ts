da51a4bb21fe19ae048eff88d96c8c63ed9ca13b
Lucas Cunha <lucas.cunha@GRUPOIV2.LOCAL>
2020-02-17 08:50:34 -0300

    redireciona simulado ao gabarito

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -38,7 +38,8 @@ const routes: Routes = [
   { path: "perguntas", component: PerguntasComponent },
   { path: "sobre", component: SobreComponent },
   { path: "depoimentos", component: DepoimentosTodosComponent },
-  { path: "simulado", component: SimuladoComponent }, // simulado aluno
+  // { path: "simulado", component: SimuladoComponent }, // simulado aluno
+  { path: "simulado", component: GabaritoSimplesComponent }, // gabaritmo
   { path: "gabarito-simples", component: GabaritoSimplesComponent }, // gabarito simples aluno
   // {
   //   path: "inscricao",
